570cee396f41f0f5f4c51db8f5e32abd624c1155
KoreaIsNotAvailable <167614055+KoreaIsNotAvailable@users.noreply.github.com>
2025-05-28 06:34:03 +0000

    1

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+변수 = st.session_state
+if '체력' not in 변수:
+    변수.체력=100
+st.image("https://static1.personalitydatabase.net/2/pdb-images-prod/3b65a9a3/profile_images/e320d616f7c243e0aff65da751303967.png")
+st.title(f"남은 체력: {변수.체력}")
+if 변수.체력 < 0 :
+    st.error("사망")
+
+if st.button("2.3만드러줘"):
+    변수.체력 = 변수.체력 - 10
+    st.rerun()
+
+if st.button("칭찬"):
+    변수.체력 = 변수.체력 + 10
+    st.rerun()
